7e3bcb4e323b51bc0f697f5465a080af1c06cb24
Boy <sivertpaulsen2@gmail.com>
2022-01-12 17:17:54 +0100

    update to newest ido

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ val idofrontVersion: String by project
 plugins {
 	id("com.mineinabyss.conventions.kotlin")
 	id("com.mineinabyss.conventions.papermc")
-	id("com.mineinabyss.conventions.slimjar")
 	id("com.mineinabyss.conventions.copyjar")
 	id("com.mineinabyss.conventions.publication")
 	kotlin("plugin.serialization")
--- src/main/kotlin/com/mineinabyss/bundles/BundlesPlugin.kt
+++ src/main/kotlin/com/mineinabyss/bundles/BundlesPlugin.kt
@@ -1,12 +1,17 @@
 package com.mineinabyss.bundles
 
 import com.mineinabyss.bundles.listeners.BundleListener
+import com.mineinabyss.idofront.platforms.IdofrontPlatforms
 import com.mineinabyss.idofront.plugin.registerEvents
 import org.bukkit.plugin.java.JavaPlugin
 
 val bundlesPlugin: BundlesPlugin by lazy { JavaPlugin.getPlugin(BundlesPlugin::class.java) }
 
 class BundlesPlugin : JavaPlugin() {
+    override fun onLoad() {
+        IdofrontPlatforms.load(this, "mineinabyss")
+    }
+
     override fun onEnable() {
         registerEvents(
             BundleListener
